fe0db741ec710c0a70003918e975447077e6ea13
kimi spencer <kimispencer@gmail.com>
2014-06-03 21:18:25 -0400

    confirmation msg

--- index.php
+++ index.php
@@ -32,8 +32,9 @@
 						<input id="SubmitBtn" type="submit" value="Sup" ng-disabled="SignupForm.$invalid" />
 					</form>
 					<section class="confirmation" ng-show="formSubmitted">
-						<p>Testing</p>
-						<h1 class="confirmation-msg">{{ signup.email }}</h1>
+						<p>Thanks, well let you know when more shit is to come.</p>
+						<p>Otherwise stay in touch, <a>info@gentlebitchclub.com</a></p>
+						<!-- <h1 class="confirmation-msg">{{ signup.email }}</h1> -->
 					</section>
 				</section>
 			</section>
