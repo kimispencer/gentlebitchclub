74fe7fea47b43ffa4330a7d4b2d18bb3c2d5797d
kimi spencer <kimispencer@gmail.com>
2014-06-03 21:27:54 -0400

    touch test again

--- js/touch.js
+++ js/touch.js
@@ -20,3 +20,6 @@ function is_touch_device() {
   return 'ontouchstart' in window // works on most browsers 
       || 'onmsgesturechange' in window; // works on ie10
 };
+
+var supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+console.log(supportsTouch)
