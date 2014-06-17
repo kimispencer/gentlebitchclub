482536c94791782e0a33c70507f2e6c195b15206
kimi spencer <kimispencer@gmail.com>
2014-06-17 17:26:52 -0400

    test test fucking test

--- process.php
+++ process.php
@@ -5,7 +5,6 @@
 	$email = $_POST['email'];
 
 	echo $_POST['email'];
-	echo '<br></br>'
 	echo "testing, " . $email;
 
 	mail( $recipient, $subject, $email, "From: $sender" ) or die ("Mail could not be sent.");
