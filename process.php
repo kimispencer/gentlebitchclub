4d062f544dedd09c63dcbfd74d2d88c802996158
kimi spencer <kimispencer@gmail.com>
2014-06-17 17:25:34 -0400

    final test?

--- process.php
+++ process.php
@@ -1,6 +1,12 @@
 <?php
+	$recipient = "kimispencer@gmail.com";
+	$sender = $recipient;
+	$subject = "Subscribe";
 	$email = $_POST['email'];
 
 	echo $_POST['email'];
+	echo '<br></br>'
 	echo "testing, " . $email;
+
+	mail( $recipient, $subject, $email, "From: $sender" ) or die ("Mail could not be sent.");
 ?>
